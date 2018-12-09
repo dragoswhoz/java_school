041728d86ee9050b712f1eaa3090c8f992649987
dragoswhoz <dragos.pdf@gmail.com>
2018-12-09 03:35:28 +0200

    sum of digits with method added

--- src/exercitii/sumOfDigitsWithMethods.java
+++ src/exercitii/sumOfDigitsWithMethods.java
@@ -5,37 +5,34 @@ import java.util.Scanner;
 public class sumOfDigitsWithMethods {
 
 	public static void main(String[] args) {
-		
+
 		Scanner sc = new Scanner(System.in);
 		System.out.println("Introduceti un numar de la tastatura");
-		
-		while(true) {
-			if(sc.hasNextInt()) {
-				
+
+		while (true) {
+			if (sc.hasNextInt()) {
 				int n = sc.nextInt();
-				int temp = n;
-				int digit = 0;
-				int sum = 0;
-				
-				while(temp != 0) {
-					digit = temp % 10;
-					sum = sum + digit;
-					temp = temp / 10;
-				}
-				
-				System.out.println("Suma cifrelor numarului " + n + " este: " + sum);
-				
+				System.out.println("Suma cifrelor numarului " + n + " este: " + calculateSumOfDigits(n));
 				break;
-				
-			}else {
+			} else {
 				System.out.println("Introduceti un numar valid");
 				sc.nextLine();
 			}
-			
+
 		}
 		sc.close();
-		
 
+	}
+
+	public static int calculateSumOfDigits(int n) {
+		int sum = 0;
+		int digit = 0;
+		while (n != 0) {
+			digit = n % 10;
+			sum = sum + digit;
+			n = n / 10;
+		}
+		return sum;
 	}
 
 }
